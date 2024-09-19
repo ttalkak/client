31669472483ce6f16c7e5ac82829d07d14838105
yunhayeon <yhy5049@naver.com>
2024-09-19 12:11:07 +0900

    feat: 이메일 인증 모달 작업

--- client/src/app/mypage/page.tsx
+++ client/src/app/mypage/page.tsx
@@ -1,114 +1,24 @@
 "use client";
 
-import { useState, useEffect, ChangeEvent } from "react";
+import React, { useState } from "react";
+import EmailRegistrationModal from "@/components/EmailRegistrationModal";
 
-export default function CallbackPage() {
-  const [email, setEmail] = useState("");
-  const [isSent, setIsSent] = useState(false);
-  const [verificationCode, setVerificationCode] = useState("");
-  const [isVerified, setIsVerified] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(300);
+export default function MyPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // 인증 유효 시간
-  useEffect(() => {
-    if (isSent && timeLeft > 0) {
-      const timer = setInterval(() => {
-        setTimeLeft(timeLeft - 1);
-      }, 1000);
-      return () => clearInterval(timer);
-    }
-  }, [isSent, timeLeft]);
-
-  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+  const openModal = () => {
+    setIsModalOpen(true);
   };
 
-  const handleCodeChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setVerificationCode(e.target.value);
-  };
-
-  const sendVerificationEmail = async () => {
-    if (!email) {
-      return;
-    }
-
-    try {
-      const response = await fetch("/api/send-verification-email", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-      });
-
-      if (response.ok) {
-        console.log("인증 메일 전송");
-        setIsSent(true);
-        setTimeLeft(300);
-      } else {
-        alert("메일 전송 실패");
-      }
-    } catch (error) {
-      console.error("Error", error);
-    }
-  };
-
-  const verifyCode = async () => {
-    try {
-      const response = await fetch("/api/verify-code", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, verificationCode }),
-      });
-
-      if (response.ok) {
-        alert("이메일 인증에 성공했습니다.");
-        setIsVerified(true);
-      } else {
-        alert("인증에 실패했습니다.");
-      }
-    } catch (error) {
-      console.error("Error", error);
-    }
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
     <div>
       <h1>마이페이지</h1>
-      
-      <div>
-        <label>이메일 주소:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-          placeholder="이메일을 입력하세요"
-          disabled={isSent}
-        />
-      </div>
-
-      <button onClick={sendVerificationEmail} disabled={isSent}>
-        인증 메일 발송
-      </button>
-
-      {isSent && (
-        <>
-          <div>
-            <label>인증 코드:</label>
-            <input
-              type="text"
-              value={verificationCode}
-              onChange={handleCodeChange}
-              placeholder="인증 코드를 입력하세요"
-            />
-          </div>
-          <div>남은 시간: {Math.floor(timeLeft / 60)}분 {timeLeft % 60}초</div>
-          <button onClick={verifyCode}>인증</button>
-          {isVerified && <div>인증이 완료되었습니다!</div>}
-        </>
-      )}
+      <button onClick={openModal}>등록하기</button>
+      <EmailRegistrationModal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
 }
