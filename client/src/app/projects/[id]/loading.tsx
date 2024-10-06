d5a901f1311b81da98706b1766b5c116bc16e5a1
junhee <limjunhee4575@gmail.com>
2024-10-06 16:06:57 +0900

    fix : 프로젝트 상세조회 스켈레톤UI 수정

--- client/src/app/projects/[id]/loading.tsx
+++ client/src/app/projects/[id]/loading.tsx
@@ -3,17 +3,19 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 export default function ProjectLoading() {
   return (
-    <div className="w-full p-8 bg-white rounded-md border mx-auto">
-      <div className="flex flex-col gap-6">
-        <div className="w-[300px]">
-          <Skeleton className="h-12" />
-        </div>
-        <div className="flex gap-6">
-          <div className="w-1/2">
-            <Skeleton className="h-48" />
-          </div>
-          <div className="w-1/2">
-            <Skeleton className="h-48" />
+    <div className="container mx-auto my-10 p-6 border rounded-lg overflow-hidden">
+      <div className="flex items-center gap-3 mb-3">
+        <Skeleton width={200} height={36} /> {/* 프로젝트 이름 */}
+        <Skeleton circle width={24} height={24} /> {/* 편집 아이콘 */}
+        <Skeleton circle width={28} height={28} /> {/* 삭제 아이콘 */}
+      </div>
+      <div className="p-6">
+        <div className="flex items-start">
+          <div className="flex-grow">
+            <div className="grid grid-cols-2 gap-4">
+              <Skeleton height={200} /> {/* FRONTEND 배포 상태 */}
+              <Skeleton height={200} /> {/* BACKEND 배포 상태 */}
+            </div>
           </div>
         </div>
       </div>
