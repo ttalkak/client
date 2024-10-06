33771e792812e7a9d5601bbbe47866df3f587924
junhee <limjunhee4575@gmail.com>
2024-10-06 15:47:33 +0900

    feat: 프로젝트 로딩상태 분리

--- client/src/components/NoSearchResult.tsx
+++ client/src/components/NoSearchResult.tsx
@@ -1,6 +1,6 @@
 import { FaSearch } from "react-icons/fa";
 
-export default function NoSearchResult({ message = "데이터가 없습니다." }) {
+export default function NoSearchResult({ message = "검색 결과가 없습니다." }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-388px)]">
       <FaSearch className="w-16 h-16 text-gray-400 mb-4" />
