6fd5bedfb69675f9217e8a499ebdcfcd5cb95bd2
limjunhee <limjunhee4575@gmail.com>
2024-10-07 09:31:12 +0900

    fix : 로그인 콜백 페이지 수정

--- client/src/app/auth/callback/page.tsx
+++ client/src/app/auth/callback/page.tsx
@@ -38,9 +38,13 @@ export default function CallbackPage() {
     fetchUserInfoAndSetState();
   }, [fetchUserInfoAndSetState]);
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-388px)]">
-      <Image src="/logo.png" alt="Logo" width={128} height={128} />
-      <h2 className="text-2xl font-bold text-gray-800 mt-8 mb-2">로그인 중</h2>
+    <div className="flex items-center justify-center min-h-[calc(100vh-268px)]">
+      <div className="flex flex-col items-center">
+        <Image src="/logo.png" alt="Logo" width={128} height={128} />
+        <h2 className="text-2xl font-bold text-gray-800 mt-8 mb-2">
+          로그인 중
+        </h2>
+      </div>
     </div>
   );
 }
