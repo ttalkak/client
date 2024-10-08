c87bbcc10b727f1f099344dd77903b188b8815bb
limjunhee <limjunhee4575@gmail.com>
2024-10-08 14:55:41 +0900

    fix : 레이아웃, 네비바 수정

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -75,7 +75,7 @@ export default function NavBar() {
     );
 
   return (
-    <nav className="bg-white border-b border-gray-200">
+    <nav className="fixed top-0 w-full bg-white border-b border-gray-200">
       <div className="px-7">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-12">
